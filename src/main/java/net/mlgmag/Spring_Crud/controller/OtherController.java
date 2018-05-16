ecb9102be02f278c5360d2061f4d47ce367e035d
Magomed Akhmedov <akhmedovmagomed2001@gmail.com>
2018-05-16 19:56:25 +0300

    Logout

--- src/main/java/net/mlgmag/Spring_Crud/controller/OtherController.java
+++ src/main/java/net/mlgmag/Spring_Crud/controller/OtherController.java
@@ -3,12 +3,18 @@ package net.mlgmag.Spring_Crud.controller;
 import net.mlgmag.Spring_Crud.model.User;
 import net.mlgmag.Spring_Crud.service.genericService.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/")
@@ -66,5 +72,16 @@ public class OtherController {
     @PostMapping("/singIn")
     public String singIn(@ModelAttribute("user") User user) {
         return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/singIn?logout";
     }
 }
